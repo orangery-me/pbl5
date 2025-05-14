93951e022aaa37757f64e8049de741ea84586ec6
Chau Thi <102220086@sv1.dut.udn.vn>
2025-05-14 15:31:37 +0700

    change data format

--- server.py
+++ server.py
@@ -12,27 +12,31 @@ firebase_admin.initialize_app(cred, {
 
 # load model
 model = load_model("LSTM_model.h5")
+new_sub_data = []
+existing_key = None
+user_id = "user_abc"
+today = datetime.now().strftime('%d/%m/%Y')  # Ví dụ: "2025-05-14"
 
 def convert_np_datetime64_to_str(np_datetime):
     py_datetime = np_datetime.astype('M8[ms]').astype(datetime)
     return py_datetime.isoformat()
 
-def send_result_to_firebase(activity, start_time, end_time, key):
-    result_ref = db.reference("result")
+# def send_result_to_firebase(activity, start_time, end_time, key):
+#     result_ref = db.reference("result")
     
-    if isinstance(start_time, np.datetime64):
-        start_time = convert_np_datetime64_to_str(start_time)
-    if isinstance(end_time, np.datetime64):
-        end_time = convert_np_datetime64_to_str(end_time)
+#     if isinstance(start_time, np.datetime64):
+#         start_time = convert_np_datetime64_to_str(start_time)
+#     if isinstance(end_time, np.datetime64):
+#         end_time = convert_np_datetime64_to_str(end_time)
     
-    result_data = {
-        "activity": activity,
-        "start_time": start_time,
-        "end_time": end_time
-    }
+#     result_data = {
+#         "activity": activity,
+#         "start_time": start_time,
+#         "end_time": end_time
+#     }
 
-    # Gửi kết quả lên Firebase, dùng start_time làm key
-    result_ref.child(str(key)).set(result_data)
+#     # Gửi kết quả lên Firebase, dùng start_time làm key
+#     result_ref.child(str(key)).set(result_data)
     
 
 def on_firebase_data(event):
@@ -80,7 +84,49 @@ def handle_data(raw_data, key):
     except Exception as e:
         print("Error:", e)
 
+def send_result_to_firebase(activity, start_time, end_time, key):
+    result_ref = db.reference("activity_records")
+    all_records = result_ref.get()
+    
+    if isinstance(start_time, np.datetime64):
+        start_time = convert_np_datetime64_to_str(start_time)
+    if isinstance(end_time, np.datetime64):
+        end_time = convert_np_datetime64_to_str(end_time)
+        
+    result_data = {
+        "activity": activity,
+        "start_time": start_time,
+        "end_time": end_time
+    }
+        
+    if all_records:
+        # Kiểm tra trong all_records có tồn tại existing key không
+        if existing_key in all_records:
+            return
+        else:
+            for _, value in all_records.items():
+                if value.get('user_id') == user_id and value.get('date') == today:
+                    existing_key = value.get('key')
+                    break
+            
+    if existing_key:
+        records_ref = result_ref.child(f"{existing_key}/records")
+        current_records = records_ref.get() or []
+        current_records.append(result_data)
+    else:
+        new_object = {
+            "user_id": user_id,
+            "date": today,
+            "records": [result_data]
+        }
+        records_ref = result_ref.push(new_object)
+        existing_key = records_ref.key
 
-new_sub_data = []
-data_ref = db.reference("dataCollection") 
-data_ref.listen(on_firebase_data)
+def main(): 
+    data_ref = db.reference("dataCollection") 
+    data_ref.listen(on_firebase_data)
+    
+    
+if __name__ == "__main__":
+    main()
+    
